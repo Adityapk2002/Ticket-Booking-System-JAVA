b0defa05eae3ea94eb2d7b81fd309d105d6d6cef
aditya khandade <adityakhandade2002@gmail.com>
2025-08-19 16:45:04 +0530

    added cancelBooking method

--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 import org.example.entities.User;
 import org.example.util.UserServiceUtil;
@@ -49,6 +50,40 @@ public class UserBookingService {
     private void saveUserListToFile() throws IOException {
         File userFile = new File(USERS_PATH);
         objectMapper.writeValue(userFile, userList);
+    }
+
+    public void fetchBookings() {
+        Optional<User> userFetched = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName())
+                    && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        if (userFetched.isPresent()) {
+            userFetched.get().printTickets();
+        }
+    }
+
+    public boolean cancelBooking(String ticketId) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your TicketID to cancel");
+        ticketId = s.next();
+
+        if (ticketId == null || ticketId.isEmpty()) {
+            System.out.println("TicketId can not be empty or null");
+            return Boolean.FALSE;
+        }
+
+        String finalTicketId1 = ticketId;
+        boolean removed = user.getTicketBooked().removeIf(ticket -> ticket.getTicketId().equals(finalTicketId1));
+
+        String finalTicketId = ticketId;
+        user.getTicketBooked().removeIf(ticket -> ticket.getTicketId().equals(finalTicketId));
+        if (removed) {
+            System.out.println("Ticket with ID :" + ticketId + "has been canceled");
+            return Boolean.TRUE;
+        } else {
+            System.out.println("No Ticket found with : " + ticketId);
+            return Boolean.FALSE;
+        }
     }
 
 }
