b287cf3546bff8f6f466113dfbac4f1c4a4f5e94
aditya khandade <adityakhandade2002@gmail.com>
2025-08-19 18:04:56 +0530

    Added some methods

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -6,6 +6,7 @@ package org.example;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.UUID;
 
@@ -38,8 +39,10 @@ public class App {
             System.out.println("7. Exit the App");
 
             option = scanner.nextInt();
+            Train trainSelectedForBooking = new Train();
 
             switch (option) {
+
                 case 1:
                     System.out.println("Enter the username to signup");
                     String nameToSignUp = scanner.next();
@@ -50,6 +53,7 @@ public class App {
                             new ArrayList<>(), UUID.randomUUID().toString());
                     userBookingService.signUp(userToSignUp);
                     break;
+
                 case 2:
                     System.out.println("Enter the username to login");
                     String nameToLogin = scanner.next();
@@ -66,6 +70,7 @@ public class App {
                         return;
                     }
                     ;
+
                 case 3:
                     userBookingService.fetchBookings();
                     break;
@@ -78,10 +83,37 @@ public class App {
                     List<Train> trains = userBookingService.getTrains(source, destination);
                     int index = 1;
                     for (Train t : trains) {
-                        System.out.println(index + "TrainId : " + getTrainId());
+                        System.out.println(index + "TrainId : " + t.getTrainId());
+                        for (Map.Entry<String, String> entry : t.getStationTimes().entrySet()) {
+                            System.out.println("Station " + entry.getKey() + " time: " + entry.getValue());
+                        }
                     }
+                    System.out.println("Select a train by typing 1,2,3....");
+                    trainSelectedForBooking = trains.get(scanner.nextInt());
                     break;
+
                 case 5:
+                    System.out.println("Select a seat out of these seats");
+                    List<List<Integer>> seats = userBookingService.fetchSeats(trainSelectedForBooking);
+                    for (List<Integer> row : seats) {
+                        for (Integer val : row) {
+                            System.out.print(val + " ");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println("Select the seat by typing the row and column");
+                    System.out.println("Enter the row");
+                    int row = scanner.nextInt();
+                    System.out.println("Enter the column");
+                    int col = scanner.nextInt();
+                    System.out.println("Booking your seat....");
+                    Boolean booked = userBookingService.bookTrainSeat(trainSelectedForBooking, row, col);
+                    if (booked.equals(Boolean.TRUE)) {
+                        System.out.println("Booked! Enjoy your journey");
+                    } else {
+                        System.out.println("Can't book this seat");
+                    }
+
                     break;
 
                 default:
